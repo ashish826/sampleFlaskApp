4009bb3cb2a22674d16c0de071920ab379a5f9e0
Ashish Aggarwal <aaggarwal081@yahoo.com>
2017-10-13 10:43:18 -0500

    modified

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'world'
 
+@app.route('/Chicago/<name>')
+def show_name(name):
+    return 'Chicago ' +name
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
