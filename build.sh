889725091e165cc0bcc7eed480577509d2736542
Ashish Aggarwal <aaggarwal081@yahoo.com>
2017-10-13 10:31:35 -0500

    modified changes

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ git clone https://github.com/ashish826/sampleFlaskApp
 cd sampleFlaskApp
 whoami
 /usr/local/bin/pip install -r requirements.txt
-docker build .
+docker build -t ashish826/sampleflaskapp .
+docker push ashish826/sampleflaskapp
+
+
