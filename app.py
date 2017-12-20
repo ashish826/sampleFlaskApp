aa312bb105415f97ef73370afa2f282e40f01f66
Ashish Aggarwal <aaggarwal081@yahoo.com>
2017-12-20 14:00:54 -0600

    made change

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def hello_world():
 def show_name(name):
     return 'Chicago ' +name
 
+@app.route('/Ashish/<last_name>')
+def name(last_name):
+    return 'Ashish ' +last_name
+
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
