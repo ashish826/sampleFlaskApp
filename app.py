3e18abcbb5532846149a2a91d4c47a6b1492bfc4
Ashish Aggarwal <aaggarwal081@yahoo.com>
2017-12-22 00:56:34 -0500

    added city

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/hello')
 def hello_world():
     return 'world'
 
+
 @app.route('/Chicago/<name>')
 def show_name(name):
-    return 'Chicago ' +name
+    return 'Chicago ' + name
+
 
 @app.route('/Ashish/<last_name>')
 def name(last_name):
-    return 'Ashish ' +last_name
+    return 'Ashish ' + last_name
 
 @app.route('/City/<city_name>')
-def city(city_name):
-    return 'City ' +city_name
+def City(city_name):
+    return 'City ' + city_name
+
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
