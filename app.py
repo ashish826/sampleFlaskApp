00399d49a62b47fa8297f5913a7f55a7bcc108bc
Ashish Aggarwal <aaggarwal081@yahoo.com>
2017-12-22 00:17:42 -0500

    added city

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ def show_name(name):
 def name(last_name):
     return 'Ashish ' +last_name
 
+@app.route('/City/<city_name>')
+def city(city_name):
+    return 'City ' +city_name
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ git clone https://github.com/ashish826/sampleFlaskApp
 cd sampleFlaskApp
 whoami
 /usr/local/bin/pip install -r requirements.txt
+cat app.py
+ls -lrt
 docker build -t ashish826/sampleflaskapp .
 docker push ashish826/sampleflaskapp
 
